ade9b3b98bbb9d90b90cddc6508ef07f38e5f5b7
LimarAryan <limarbizz@gmail.com>
2024-02-18 10:34:26 -0500

    Added hello_world and simple_calculator as starting projects

---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+number_one = int(input("Enter your first value: "))
+number_two = int(input("Enter your second value: "))
+
+def math_operation(number_one, number_two):
+    operation = input("Enter which math operation (add, sub, mult, div) you'd like: ")
+    if(operation == "add"):
+        return number_one + number_two
+    elif(operation == "sub"):
+        return number_one - number_two
+    elif(operation == "mult"):
+        return number_one * number_two
+    elif(operation == "div"):
+        return number_one / number_two
+
+print(math_operation(number_one, number_two))
