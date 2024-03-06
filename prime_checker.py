6cb95bc733479e0acee51e126e61561b4a3eb1c0
LimarAryan <limarbizz@gmail.com>
2024-03-06 01:44:19 -0500

    prime checker program

--- prime_checker.py
+++ prime_checker.py
@@ -11,4 +11,26 @@ def is_prime_a(n):
     return True
 
 
+def is_prime_b(n):
+    if n > 1:
+        if n == 2:
+            return True
+        else:
+            for i in range(2, n):
+                if n % i == 0:
+                    return False 
+            return True
+    return False
+
+
+def is_prime_c(n):
+    divisible = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisible += 1
+    if divisible == 2:
+        return True
+    return False
+
+
 print(is_prime_a(int(input("Enter a number to check: "))))
